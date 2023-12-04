3a4ae37cfc0f559a73b79f13d0af5e3a26f6bd26
diosamuel <koding12sam@gmail.com>
2023-12-04 21:50:41 +0700

    verif face

--- Pendaftaran.py
+++ Pendaftaran.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 import time
+import cv2
 from datetime import datetime
 from alat.db import Database
-
+# import random
+# randnumber = random.randint(1, 100)
 st.set_page_config(
     page_title="PEMILU",
     page_icon="💪",  # You can use an emoji as the logo
@@ -12,6 +14,7 @@ st.set_page_config(
 
 st.sidebar.title("Pemilihan Umum")
 st.sidebar.write("Pemilu adalah singkatan dari Pemilihan Umum, yang merupakan suatu proses dimana warga negara suatu negara memberikan suara mereka untuk memilih para wakil mereka dalam pemerintahan. Pemilu merupakan salah satu prinsip dasar demokrasi yang memungkinkan rakyat untuk berpartisipasi dalam menentukan pemimpin dan kebijakan negara.")
+
 class RegistrasiVoting:
 	def __init__(self):
 		self.ProvinsiIndonesia = ['Aceh', 'Sumatera Barat', 'Yogyakarta', 'Sumatera Utara', 'Bangka-Belitung', 'Papua Barat', 'Jawa Timur', 'Kalimantan Barat', 'Kalimantan Selatan', 'Kalimantan Timur', 'Kepulauan Riau', 'Lampung', 'Maluku', 'Maluku Utara', 'Nusa Tenggara Barat', 'Nusa Tenggara Timur', 'Papua', 'Riau', 'Sulawesi Selatan', 'Bengkulu', 'Sulawesi Tengah', 'Sulawesi Utara', 'Sulawesi Tenggara', 'Bali', 'Banten', 'Gorontalo', 'Jakarta Raya', 'Jambi', 'Jawa Barat', 'Jawa Tengah', 'Kalimantan Tengah', 'Sulawesi Barat', 'Sumatera Selatan', 'Kalimantan Utara']
@@ -60,8 +63,13 @@ class RegistrasiVoting:
 		st.write(f"Kelamin : {self.gender}")
 		st.write(f"Provinsi : {self.provinsi}")
 		st.write(f"Waktu : {self.waktu}")
-		# st.write(self.kamera)
-		st.image(self.kamera)
+
+		#img detection
+		imagePath = 'test.jpg'
+		img = cv2.imread(imagePath)
+		img_with_faces = detect_faces(img)
+		st.image(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), caption="Wajah Pemilih")
+		# st.image(self.kamera)
 
 	# Layouting
 	def buatFormulir(self):
@@ -76,7 +84,7 @@ class RegistrasiVoting:
 
 		formContainer = {
 			"nama":self.temp_con["nama"].text_input('Nama Lengkap'),
-			"nik":self.temp_con["nik"].text_input('NIK'),
+			"nik":self.temp_con["nik"].number_input('NIK'),
 			"gender":self.temp_con["gender"].selectbox("Jenis kelamin",["Laki-Laki","Perempuan"]),
 			"provinsi":self.temp_con["provinsi"].selectbox("Pilih Provinsi",self.ProvinsiIndonesia),
 			"camera":self.temp_con["camera"].camera_input("Verifikasi Wajah"),
@@ -91,7 +99,7 @@ class RegistrasiVoting:
 			self.temp_con[i].empty()
 
 	def aturFormulir(self,container):
-		if self.nama is not None:
+		if self.apakahTerverifikasi():
 			self.hapusContainer()
 			self.tampilBiodata()
 		else:
@@ -112,15 +120,33 @@ class RegistrasiVoting:
 				}
 
 				st.session_state.kamera = self.kamera
-				loading = st.success("Loading...")
-				time.sleep(1)
-				loading.empty()
-				#hapus semua element
-				self.sudahVerifikasi()
-				self.hapusContainer()
-				self.tampilBiodata()
+
+				with open('test.jpg','wb') as file:
+					file.write(self.kamera.getbuffer())
+				imagePath = 'test.jpg'
+				img = cv2.imread(imagePath)
+				img_with_faces = detect_faces(img)
+
+				if img_with_faces["isVerified"]:
+					time.sleep(1)
+					#hapus semua element
+					self.sudahVerifikasi()
+					self.hapusContainer()
+					self.tampilBiodata()
+				else:
+					st.warning("Wajah kurang jelas, silahkan foto ulang")
 
 
+def detect_faces(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return {
+        "image":image,
+        "isVerified":bool(len(faces))
+    }
 
 # Layout
 st.title("Pendaftaran E-Voting Pemilu")
